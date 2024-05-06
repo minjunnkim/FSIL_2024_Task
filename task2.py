9b7f04dc356dfdfbc4a6203d9d24551617a0abb8
Minjun Kim <ckandrew04@gmail.com>
2024-05-06 02:27:33 +0200

    task 2 testing

--- task2.py
+++ task2.py
@@ -1,37 +1,33 @@
 import streamlit as st
 from .task1 import company_process, keyword_tracking, visualize_section_length, analyze_company_data, visualize_sentiments
 
-def main():
-    st.title('10-K Filing Analysis App')
+st.title('10-K Filing Analysis App')
 
-    company = st.selectbox(
-        "Choose a company",
-        ['META', 'AAPL', 'MSFT', 'AMZN', 'GOOGL'],
-        index=0
-    )
+company = st.selectbox(
+    "Choose a company",
+    ['META', 'AAPL', 'MSFT', 'AMZN', 'GOOGL'],
+    index=0
+)
 
-    company_process(company)
+company_process(company)
 
-    analysis_type = st.selectbox(
-        "Select Analysis Type",
-        ['Keyword Tracking', 'Visualize Section Length', 'Sentiment Analysis'],
-        index=0
-    )
+analysis_type = st.selectbox(
+    "Select Analysis Type",
+    ['Keyword Tracking', 'Visualize Section Length', 'Sentiment Analysis'],
+    index=0
+)
 
-    if st.button('Analyze'):
-        if analysis_type == 'Keyword Tracking':
-            keywords = st.text_input("Enter keywords separated by commas", "cybersecurity, data privacy, sustainability")
-            keyword_list = [keyword.strip() for keyword in keywords.split(',')]
-            fig = keyword_tracking(company, keyword_list) 
-            st.pyplot(fig)
-        elif analysis_type == 'Visualize Section Length':
-            item_key = st.text_input("Enter the item key", "item 1a. ")
-            fig = visualize_section_length(company, item_key) 
-            st.pyplot(fig)
-        elif analysis_type == 'Sentiment Analysis':
-            results = analyze_company_data(company)
-            fig = visualize_sentiments(company, results)
-            st.pyplot(fig)
-
-if __name__ == "__main__":
-    main()
+if st.button('Analyze'):
+    if analysis_type == 'Keyword Tracking':
+        keywords = st.text_input("Enter keywords separated by commas", "cybersecurity, data privacy, sustainability")
+        keyword_list = [keyword.strip() for keyword in keywords.split(',')]
+        fig = keyword_tracking(company, keyword_list) 
+        st.pyplot(fig)
+    elif analysis_type == 'Visualize Section Length':
+        item_key = st.text_input("Enter the item key", "item 1a. ")
+        fig = visualize_section_length(company, item_key) 
+        st.pyplot(fig)
+    elif analysis_type == 'Sentiment Analysis':
+        results = analyze_company_data(company)
+        fig = visualize_sentiments(company, results)
+        st.pyplot(fig)
